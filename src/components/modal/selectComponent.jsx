9ae6123fc4ee132087719360eaec7ad07639b817
JerryCodesAfrica <egemonyeemelie@gmail.com>
2024-09-23 09:43:16 +0100

    color adjustement

--- src/components/modal/selectComponent.jsx
+++ src/components/modal/selectComponent.jsx
@@ -20,13 +20,13 @@ function Comps({
   // console.log(change)s
 
   return (
-    <div className="border-gray-300 w-auto max-h-72 rounded-lg p-2 py-6 border-2">
+    <div className="border-gray-300 w-auto max-h-72 rounded-lg p-2 py-3 border-2">
       <div className="flex flex-col gap-2 relative">
         <div className="flex lg:pl-0 pl-10 flex-row gap-2">
           <input
             type="radio"
             onChange={changed}
-            className="accent-green-600"
+            className="accent-[#3DB5AB]"
             name="scale"
             id={ids}
             ref={radioRef}
@@ -47,19 +47,19 @@ function Comps({
 
       {isSelected && (
         <div
-          className={`flex justify-between px-6 gap-2 items-center lg:py-3 flex-col lg:flex-row border-t-2 lg:mt-6`}
+          className={`flex justify-between px-6 gap-2 items-center lg:py-3 flex-col lg:flex-row border-t-2 lg:mt-2`}
           key={ids}
         >
-          <h2 className="text-gray-400">Enter your pledge</h2>
-          <div className="flex flex-row lg:gap-0 justify-evenly w-10/12">
+          <h2 className="text-gray-400 lg:self-center">Enter your pledge</h2>
+          <div className="flex flex-row lg:justify-end lg:gap-3 justify-evenly lg:w-4/12">
             <input
               placeholder="$"
-              className="rounded-full border-gray-400 border-[1px] w-5/12 lg:w-4/12 p-2"
+              className="rounded-full border-gray-400 border-[1px] w-5/12 lg:w-[50%] p-2"
               type="text"
               name=""
               id=""
             />
-            <button className="text-white bg-green-500 px-6 p-2 rounded-full text-[14px]">
+            <button className="text-white lg:w-[50%] bg-[#3DB5AB] px-6 p-2 rounded-full text-[14px]">
               Continue
             </button>
           </div>
